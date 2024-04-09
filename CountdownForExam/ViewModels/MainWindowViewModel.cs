8a6a1a054d0046e15e048ebd81579bea2441aed9
zhangran <dons2023@126.com>
2024-04-09 09:23:31 +0800

    添加项目文件。

---
+++ CountdownForExam/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CountdownForExam.Views;
+using Prism.Mvvm;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace CountdownForExam.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private string _Text;
+        public string Text
+        {
+            get { return _Text; }
+            set { SetProperty(ref _Text, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            var target = new DateTime(2024, 5, 24);
+
+
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(1000 * 60 * 60);
+                    Text = (target - DateTime.Now).TotalDays.ToString();
+                }
+            });
+        }
+    }
+}
