0a24d8fd8f05764f52362d41ba1a64fbd51b5ad8
zhangran <dons2023@126.com>
2024-04-10 18:17:28 +0800

    [D]clean code.

--- CountdownForExam/Views/MainWindow.xaml.cs
+++ CountdownForExam/Views/MainWindow.xaml.cs
@@ -16,21 +16,15 @@ namespace CountdownForExam.Views
         {
             InitializeComponent();
 
-            // 创建 NotifyIcon
             notifyIcon = new NotifyIcon
             {
                 Icon = new Icon("countdown.ico", 32, 32),
                 Visible = true,
-                Text = "Countdown" // 鼠标悬停时显示的文本
+                Text = "Countdown" 
             };
-
-            // 创建右键菜单
             var contextMenu = new ContextMenuStrip();
             contextMenu.Items.Add("Exit", null, (s, e) => { this.Close(); });
-
             notifyIcon.ContextMenuStrip = contextMenu;
-
-            // 确保在应用程序退出时清理图标
             this.Closed += (s, e) => { notifyIcon.Visible = false; };
         }
     }
