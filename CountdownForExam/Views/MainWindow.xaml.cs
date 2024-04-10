fc5d4e15b9ef9d8251bef86948fd58631ca72b1c
zhangran <dons2023@126.com>
2024-04-10 17:20:33 +0800

    [A]set in sys task bar.

--- CountdownForExam/Views/MainWindow.xaml.cs
+++ CountdownForExam/Views/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System.Windows;
+using System.Windows.Forms;
+using System.Drawing;
+using System;
+
 
 namespace CountdownForExam.Views
 {
@@ -7,9 +11,27 @@ namespace CountdownForExam.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+        private NotifyIcon notifyIcon;
         public MainWindow()
         {
             InitializeComponent();
+
+            // 创建 NotifyIcon
+            notifyIcon = new NotifyIcon
+            {
+                Icon = new Icon(System.Windows.Application.GetResourceStream(new Uri("pack://application:,,,/countdown.ico")).Stream),
+                Visible = true,
+                Text = "Countdown" // 鼠标悬停时显示的文本
+            };
+
+            // 创建右键菜单
+            var contextMenu = new ContextMenuStrip();
+            contextMenu.Items.Add("Exit", null, (s, e) => { this.Close(); });
+
+            notifyIcon.ContextMenuStrip = contextMenu;
+
+            // 确保在应用程序退出时清理图标
+            this.Closed += (s, e) => { notifyIcon.Visible = false; };
         }
     }
 }
